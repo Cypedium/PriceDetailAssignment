54de4c0165eaf5d2d80aee8f8eb66892ec90a49f
Mikael Aurell <aurell.mikael@gmail.com>
2020-09-16 10:11:07 +0200

    Added ParsingService

--- PriceDetailAssignment/Models/DbInitializer.cs
+++ PriceDetailAssignment/Models/DbInitializer.cs
@@ -6,6 +6,7 @@ using System.Runtime.CompilerServices;
 using TinyCsvParser;
 using System.Text;
 using TinyCsvParser.Mapping;
+using PriceDetailAssignment.Models.Services;
 
 namespace PriceDetailAssignment.Models
 {
@@ -16,66 +17,13 @@ namespace PriceDetailAssignment.Models
             // Check if our database is created if not then create it
             context.Database.EnsureCreated();
 
-            //Parsing CSV-FILE-----------------------------------------------------------------------------------
+            //Adding list of products to database-------------------------------------------------------------------------------
             if (!context.Products.Any())
             {
-            CsvParserOptions csvParserOptions = new CsvParserOptions(true, '\t'); //true=skippa headern
-            var csvParser = new CsvParser<Product>(csvParserOptions, new CsvProductMapping());
-            var records = csvParser.ReadFromFile("price_detail.csv", Encoding.UTF8)
-                .Where(x => x.IsValid) //Parse Only the valid rows
-                .ToList();
-           
-                //Change type, convert Raw data and save to Database-------------------------------------------------
-                List<Product> products = new List<Product>();
-
-                if (!context.Products.Any())
-                {
-                    foreach (CsvMappingResult<Product> row in records) //vilken kolumn
-                    {
-                        Product product = new Product()
-                        {
-                            PriceValuedId = row.Result.PriceValuedId,
-                            Created = row.Result.Created,
-                            Modified = row.Result.Modified,
-                            CatalogEntryCode = row.Result.CatalogEntryCode,
-                            MarketId = row.Result.MarketId,
-                            CurrencyCode = row.Result.CurrencyCode,
-                            ValidFrom = row.Result.ValidFrom,
-                            ValidUntil = row.Result.ValidUntil,
-                            UnitPrice = row.Result.UnitPrice
-                            //UnitPrice = Convert.ToDecimal(row.Result.UnitPrice)
-                        };
-                        products.Add(product);
-                    }
-
-                    //ska detta vara med?-----------------------------------------------------------
-
-                    //List<Product> products_sortingByCatalogEntryCode = new List<Product>();
-                    //products_sortingByCatalogEntryCode = products;
-                    //products_sortingByCatalogEntryCode.Sort(delegate (Product x, Product y)
-                    //products.Sort(delegate (Product x, Product y)
-                    //{
-                    //    if (x.CatalogEntryCode == null && y.CatalogEntryCode == null) return 0;
-                    //    else if (x.CatalogEntryCode == null) return -1;
-                    //    else if (y.CatalogEntryCode == null) return 1;
-                    //    else return x.CatalogEntryCode.CompareTo(y.CatalogEntryCode);
-                    //});
-
-                    //-----------------------------------------------------------------------------
-
-                    //Testkod?----------------------------------------------------------------------
-                    var products2 = products;
-                    products2.
-                    GroupBy(w => w.CatalogEntryCode).
-                    Select(g => new { CatalogEntryCode = g.Key, Products2 = g }).
-                    OrderBy(o => o.CatalogEntryCode).ToList();
-                    //Select(g => new { Length = g.Key, Words = g }).
-                    //--------------------------------------------------------------------------------
-
-                    context.Products.AddRange(products);
-                    context.SaveChanges();
-                }
-            }   
+                context.Products.AddRange(ParsingService.CsvToProductList("price_detail.csv"));
+                context.SaveChanges();
+            }
+            //-------------------------------------------------------------------------------------------------------------------
         }
     }
 }
---
+++ PriceDetailAssignment/Models/Services/ParsingService.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TinyCsvParser;
+using TinyCsvParser.Mapping;
+
+namespace PriceDetailAssignment.Models.Services
+{
+    public class ParsingService
+    {
+        //Parsing price_detail.csv to List of products using TinyCsvParser-----------------------------------------
+        public static List<Product> CsvToProductList(string fileName)
+        {
+            List<Product> products = new List<Product>();
+
+            if (string.IsNullOrWhiteSpace(fileName))
+            {
+                return products;
+            }
+
+            CsvParserOptions csvParserOptions = new CsvParserOptions(true, '\t'); //true=skippa headern
+            var csvParser = new CsvParser<Product>(csvParserOptions, new CsvProductMapping());
+            var records = csvParser.ReadFromFile("price_detail.csv", Encoding.UTF8)
+                .Where(x => x.IsValid) //Parse Only the valid rows
+                .ToList();
+
+            //Change type, convert Raw data and save to Database-------------------------------------------------
+
+            foreach (CsvMappingResult<Product> row in records) //vilken kolumn
+            {
+                Product product = new Product()
+                {
+                    PriceValuedId = row.Result.PriceValuedId,
+                    Created = Convert.ToDateTime(row.Result.Created),
+                    Modified = Convert.ToDateTime(row.Result.Modified),
+                    CatalogEntryCode = Convert.ToString(row.Result.CatalogEntryCode),
+                    MarketId = Convert.ToString(row.Result.MarketId),
+                    CurrencyCode = Convert.ToString(row.Result.CurrencyCode),
+                    ValidFrom = Convert.ToDateTime(row.Result.ValidFrom),
+                    ValidUntil = Convert.ToDateTime(row.Result.ValidUntil),
+                    UnitPrice = Convert.ToDecimal(row.Result.UnitPrice)
+                };
+                products.Add(product);
+            }
+            return products;
+        }           
+    }
+}
+
+//Test sort working fine-----------------------------------------------------------
+
+//List<Product> products_sortingByCatalogEntryCode = new List<Product>();
+//products_sortingByCatalogEntryCode = products;
+//products_sortingByCatalogEntryCode.Sort(delegate (Product x, Product y)
+//products.Sort(delegate (Product x, Product y)
+//{
+//    if (x.CatalogEntryCode == null && y.CatalogEntryCode == null) return 0;
+//    else if (x.CatalogEntryCode == null) return -1;
+//    else if (y.CatalogEntryCode == null) return 1;
+//    else return x.CatalogEntryCode.CompareTo(y.CatalogEntryCode);
+//});
+
+//-----------------------------------------------------------------------------
+
+//Testkod?----------------------------------------------------------------------
+//var products2 = products;
+//products2.
+//GroupBy(w => w.CatalogEntryCode).
+//Select(g => new { CatalogEntryCode = g.Key, Products2 = g }).
+//OrderBy(o => o.CatalogEntryCode).ToList();
+//Select(g => new { Length = g.Key, Words = g }).
+//--------------------------------------------------------------------------------
